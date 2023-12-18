2e644aac7096257eb3c3ff70105a8bf022c0ffa5
Rahul <rahulk_c21124@students.isquareit.edu.in>
2023-12-18 16:44:54 +0530

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -15,3 +15,24 @@ public:
          return ans;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i = 0; i<nums.size()-1;i++){
+            int j = i+1;
+            while(j<nums.size()){
+                
+            
+          if(nums[i]+nums[j]==target){
+                    ans.push_back(min(i,j));
+                    ans.push_back(max(i,j));
+                }
+            j++;
+            }
+        }
+         return ans;
+    }
+};
