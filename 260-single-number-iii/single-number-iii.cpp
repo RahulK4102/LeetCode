3f7b271dbcebea5e3508328c41c1a4403e2300c8
Rahul <rahulk_c21124@students.isquareit.edu.in>
2024-07-15 18:43:27 +0530

    Time: 10 ms (41.85%) | Memory: 12.6 MB (31.06%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -2,17 +2,22 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         vector<int> ans;
-        for(int i =0;i<nums.size();i++){
-            int count = 0;
-            for(int j =0; j<nums.size();j++){
-                if(i!=j && nums[i]==nums[j]){
-                    count++;
-                }
-            }
-            if(count<1){
-                ans.push_back(nums[i]);
+        long long XOR = 0;
+        int a = 0;
+        int b = 0;
+        for(int i = 0; i<nums.size(); i++){
+            XOR ^=nums[i];
+        }
+        int rightMostBit = (XOR & (XOR-1))^XOR;
+        for(int i = 0; i<nums.size(); i++){
+            if(rightMostBit&nums[i]){
+                a ^= nums[i];
+            }else{
+                b ^= nums[i];
             }
         }
-        return ans;
+        ans.push_back(a);
+        ans.push_back(b);
+    return ans;
     }
 };
