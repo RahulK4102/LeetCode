bc2387368e13969600f43c28487dd11fcbdf0202
Rahul <rahulk_c21124@students.isquareit.edu.in>
2024-07-18 00:07:17 +0530

    Time: 103 ms (32.72%) | Memory: 75.6 MB (10.34%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int,int> ans;
+        unordered_map<int,int> ans;
         for(int i =0; i<nums.size(); i++){
             ans[nums[i]]++;
         }
