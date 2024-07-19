902aaa024ff8602192a69ec93716e12d0dc57b9e
Rahul <rahulk_c21124@students.isquareit.edu.in>
2024-07-19 23:57:19 +0530

    Time: 43 ms (5.16%) | Memory: 29.6 MB (5.10%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i = 0 ; i<nums.size(); i++){
-            if(nums[i]!=i){
-                return i;
+        map<int,int> ans;
+        for(int  i = 0 ; i<=nums.size(); i++){
+            ans[i] = 0;
+        }
+        for(int  i = 0 ; i<nums.size(); i++){
+            ans[nums[i]] = 1;
+        }
+        for(auto it:ans){
+            if(it.second==0){
+                return it.first;
             }
         }
-        return nums.size();
+        return 0 ;
     }
 };
